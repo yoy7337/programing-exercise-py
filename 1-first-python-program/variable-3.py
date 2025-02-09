46415e06fcbfce9c5c190a4aaba7d0c661391a7e
WeiChun Horng <yoy7337@gmail.com>
2025-02-09 18:16:28 +0800

    doc: add ch1 for variable and ch2 for string.

---
+++ 1-first-python-program/variable-3.py
@@ -0,0 +1,15 @@
+## try to swap a and b
+'''
+a = 1
+b = 'str'
+'''
+
+a = 1
+b = 'str'
+
+c = a
+a = b
+b = c
+
+print(f"a={a}")
+print(f"b={b}")
---
+++ homework/1-reverse-string.py
@@ -0,0 +1,8 @@
+'''
+反轉字串
+使用 forloop
+不要使用 string[::-1]
+apple -> elppa
+database -> esabatab
+'''
+
