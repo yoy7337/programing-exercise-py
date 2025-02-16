ef3a6845fbbc19c26cc3c184266c8848cd4088e1
WeiChun Horng <yoy7337@gmail.com>
2025-02-16 08:24:27 +0800

    feat: add func loop cond...

--- homework/1-reverse-string.py
+++ homework/1-reverse-string.py
@@ -6,3 +6,11 @@ apple -> elppa
 database -> esabatab
 '''
 
+str = input("Please input string, we will reverse it: ")
+reversed_str = ""
+
+for i in range(0, len(str)):
+  # print(str[len(str) - i - 1])
+  reversed_str = reversed_str + (str[len(str) - i - 1])
+
+print(reversed_str)
